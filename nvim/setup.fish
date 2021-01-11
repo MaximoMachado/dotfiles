eab0b48f298dc2280e6c22d753cc1cdacaa02968
Maximo Machado <maximomachado246@gmail.com>
2021-01-10 19:33:50 -0500

    Add automatic setup for nvim plugin manager

--- nvim/setup.fish
+++ nvim/setup.fish
@@ -4,6 +4,10 @@
 sudo pip3 install pynvim
 sudo npm -g install neovim
 
+# Installs plugin manager
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
 echo 'Neovim Setup has ended.'
 echo 'Run :PlugInstall in nvim to install plugins.'
 echo 'Install clangd in your preferred package manager to use the corresponding COC extension.'
