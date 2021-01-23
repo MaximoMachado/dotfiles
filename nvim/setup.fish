6564876e48d941736722bbe23f9fb865996ce8ad
Maximo Machado <maximomachado246@gmail.com>
2021-01-22 22:21:12 -0500

    Download font from Jet Brains website rather than git repo

--- nvim/setup.fish
+++ nvim/setup.fish
@@ -9,23 +9,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 # Install Nerd Font
 mkdir -p ~/.local/share/fonts/nerd-fonts/jetbrains-mono
-cd ~/.local/share/fonts/nerd-fonts/jetbrains-mono && curl -fLo 'JetBrains Mono Regular Nerd Font Complete Mono.ttf' https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
-
-cd ~/.local/share/fonts/nerd-fonts/jetbrains-mono && curl -fLo 'JetBrains Mono Medium Nerd Font Complete Mono.ttf' https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Medium/complete/JetBrains%20Mono%20Medium%20Nerd%20Font%20Complete%20Mono.ttf
-
-cd ~/.local/share/fonts/nerd-fonts/jetbrains-mono && curl -fLo 'JetBrains Mono Medium Italic Nerd Font Complete Mono.ttf' https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Medium-Italic/complete/JetBrains%20Mono%20Medium%20Italic%20Nerd%20Font%20Complete%20Mono.ttf
-
-
-cd ~/.local/share/fonts/nerd-fonts/jetbrains-mono && curl -fLo 'JetBrains Mono Italic Nerd Font Complete Mono.ttf' https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Italic/complete/JetBrains%20Mono%20Italic%20Nerd%20Font%20Complete%20Mono.ttf
-
-
-cd ~/.local/share/fonts/nerd-fonts/jetbrains-mono && curl -fLo 'JetBrains Mono ExtraBold Nerd Font Complete Mono.ttf' https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/ExtraBold/complete/JetBrains%20Mono%20ExtraBold%20Nerd%20Font%20Complete%20Mono.ttf
-
-cd ~/.local/share/fonts/nerd-fonts/jetbrains-mono && curl -fLo 'JetBrains Mono ExtraBold Italic Nerd Font Complete Mono.ttf' https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/ExtraBold-Italic/complete/JetBrains%20Mono%20ExtraBold%20Italic%20Nerd%20Font%20Complete%20Mono.ttf
-
-cd ~/.local/share/fonts/nerd-fonts/jetbrains-mono && curl -fLo 'JetBrains Mono Bold Nerd Font Complete Mono.ttf' https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Bold/complete/JetBrains%20Mono%20Bold%20Nerd%20Font%20Complete%20Mono.ttf
-
-cd ~/.local/share/fonts/nerd-fonts/jetbrains-mono && curl -fLo 'JetBrains Mono Bold Italic Nerd Font Complete Mono.ttf' https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Bold-Italic/complete/JetBrains%20Mono%20Bold%20Italic%20Nerd%20Font%20Complete%20Mono.ttf
+cd ~/.local/share/fonts/nerd-fonts/jetbrains-mono && curl -fLo 'JetBrainsMono.zip' https://download.jetbrains.com/fonts/JetBrainsMono-2.221.zip && unzip 'JetBrainsMono.zip' && rm -f 'JetBrainsMono.zip'
 
 echo 'Run :PlugInstall in nvim to install plugins.'
 echo 'Install clangd in your preferred package manager to use the corresponding COC extension.'
