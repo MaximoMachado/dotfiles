8f47a4329cb2389904a91c0dc3c888265a3b8a84
Maximo Machado <maximomachado246@gmail.com>
2021-08-18 20:46:31 -0400

    Add symbolic link script and extra config files

---
+++ link-configs.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+ln ~/.config/.gitconfig ~/.gitconfig -s
+
+ln ~/.config/tmux/.tmux.conf ~/.tmux.conf -s
+
+ln ~/.config/.zshrc ~/.zshrc -s
+
+ln ~/.config/nvim/.vimrc ~/.vimrc -s
+
+ln ~/.config/.ssh ~/.ssh -s
