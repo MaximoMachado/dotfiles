8dfab22f00800848db0399b72b280ff22120171d
Maximo Machado <maximomachado246@gmail.com>
2021-08-18 21:21:47 -0400

    Add oh-my-zsh .zshrc

---
+++ setup-files/zsh-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/env sh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"c
--- setup.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-
-# TODO Setup functions for user confirmation and using their preferred package manager
-./fish/setup.fish
-./nvim/setup.fish
-./tmux/setup.fish
-./fusuma/setup.fish
-echo 'Dot Files setup has ended. Please restart your shell.' 
